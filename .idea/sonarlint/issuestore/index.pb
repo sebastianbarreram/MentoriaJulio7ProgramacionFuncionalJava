484321f70aa28869aa089485dfd6b287b7359f23
unknown <sbarrera.96@hotmail.com>
2022-07-07 17:51:31 -0500

    Images for README file

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,7 @@
 
 7
 pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+t
+Dsrc/main/java/co/com/mentoria/programacionfuncional/Calculadora.java,a\d\ad78e26763043404f301f4aede080607259688ed
+m
+=src/main/java/co/com/mentoria/programacionfuncional/Main.java,c\5\c5326f5ffdfd09b73273ddb9298fab645f613089
--- src/main/java/co/com/mentoria/programacionfuncional/Main.java
+++ src/main/java/co/com/mentoria/programacionfuncional/Main.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Double> listaA = Arrays.asList(7.0, 2.0, 6.0, 3.0);
-        List<Double> listaB = Arrays.asList(8.0, 9.0, 2.0, 5.0, 6.0);
+        List<Double> listaB = Arrays.asList(8.0, 9.0, 2.0);
         Calculadora calculadora = new Calculadora(listaA, listaB);
         System.out.println("El resultado de sumar es: " + calculadora.sumar());
         System.out.println("El resultado de restar es: " + calculadora.restar());
